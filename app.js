84d0b4358486e3b3d52e07e75a365ff843a1a520
jacton jastice <jactonjastice0001@gmail.com>
2026-08-20 01:26:19 +0300

    add uncaught exception logging

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const express = require('express');
+process.on('uncaughtException', (err) => { console.error('UNCAUGHT EXCEPTION:', err)
+process.exit(1); })
+    const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
